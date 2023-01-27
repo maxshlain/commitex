ab9e05c54fc8eeb02eab2cbbc03fc338164abc74
maxshlain <maxshlain@gmail.com>
2023-01-27 18:40:03 +0200

    Add Serilog logging and configure global logger

--- src/Commitex.Console/Program.cs
+++ src/Commitex.Console/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Commitex.Console;
+﻿using Serilog;
+
+namespace Commitex.Console;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        await new ConsoleApp().Main(args);
+        using var log = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+        Log.Logger = log;
+        Log.Information("The global logger has been configured");
+        
+        try
+        {
+            await new ConsoleApp().Main(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Something went wrong");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
