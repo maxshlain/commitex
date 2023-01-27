cb4dfdacb4a01fdfbc209c5bd3af1e6e65e806f8
maxshlain <maxshlain@gmail.com>
2023-01-27 17:36:06 +0200

    Add settings and diff reader to Program.cs

--- Commitex.Console/Commitex.Console/Program.cs
+++ Commitex.Console/Commitex.Console/Program.cs
@@ -1,16 +1,42 @@
-﻿using Commitex.Console;
+﻿using System.Security.Cryptography;
+using Commitex.Console;
+using Commitex.Core;
 using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+public class Settings
+{
+    public string Token { get; set; }
+    public string Url { get; set; }
+}
 
-IConfiguration configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", false, true)
-    .AddJsonFile("appsettings.Development.json", false, true)
-    .Build();
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        string diff;
+        using (var reader = new StreamReader(Console.OpenStandardInput()))
+        {
+            diff = reader.ReadToEnd();
+        }
+        
+        Console.WriteLine(diff);
 
-var token = configuration.GetSection("Token").Value;
-var predictor = new Commitex.Core.Predictor(token);
-var prompt = "Propose a git commit message for this diff:\n\n" + new DiffSource().Source;
-var prediction = await predictor.PredictAsync(prompt);
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile("appsettings.Development.json", false, true)
+            .Build();
 
-Console.WriteLine(prediction);
+        Settings settings = configuration.GetSection("Settings").Get<Settings>()
+            ?? throw new Exception("Settings not found");
+        
+        var prompt = "Propose a git commit message for this diff:\n\n" + diff;
+
+        var predictor = new Commitex.Core.Predictor(settings.Token);
+
+        var prediction = await predictor.PredictAsync(prompt);
+
+        Console.WriteLine(prediction);
+    }
+}
