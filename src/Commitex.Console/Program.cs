6c1b0e0b8fd2f5bea4be9e4bce8e1c366037ce97
maxshlain <maxshlain@gmail.com>
2023-01-28 19:54:35 +0200

    Add verbose option to Commitex Console App

--- src/Commitex.Console/Program.cs
+++ src/Commitex.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using Serilog;
+﻿using System.CommandLine;
+using Serilog;
+using Serilog.Events;
 
 namespace Commitex.Console;
 
@@ -6,12 +8,34 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateLogger();
+        var verbose = new Option<bool>(
+            name: "--verbose",
+            description: "Verbose output",
+            getDefaultValue: () => false);
 
+        var root = new RootCommand("Commitex - let AI write better commit messages (better than you)");
+        root.AddOption(verbose);
+        
+        root.SetHandler(
+            (verboseArg) => MainImpl(verboseArg, args),
+            verbose
+        );
+        
+        await root.InvokeAsync(args);
+    }
+
+    private static async Task MainImpl(bool verbose, string[] args)
+    {
         try
         {
+            LogEventLevel consoleLevel = verbose ? LogEventLevel.Verbose : LogEventLevel.Information;
+            LogEventLevel fileLevel = verbose ? LogEventLevel.Verbose : LogEventLevel.Debug;
+            
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(consoleLevel)
+                .WriteTo.File("log-.txt", fileLevel, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             var diffProvider = new DiffProvider();
             var app = new ConsoleApp(diffProvider);
             await app.Main(args);
